b35971c3cec2d249efd2f985d55601a3a2aa8664
Dominik Kern <dominik.kern.ifgt@gmail.com>
2021-01-14 16:37:45 +0100

    variables double-checked

--- staggered_hm.py
+++ staggered_hm.py
@@ -1,6 +1,7 @@
 # TODO 
 #    monitor convergence
 #    verification of case 1 and 2 [Kim09] (then involve Tengfei)
+#    roller BC leftright
 #    quadliteral mesh
 #    monolithic formulation
 """
@@ -10,30 +11,33 @@ temporal Euler-backward discretization
 """
 from __future__ import print_function
 import fenics as fe
+import numpy as np
+
+fe.set_log_level(30)  # control info/warning/error messages
 
 ## input variables
 # geometry
-Length = 15;    # 0...yMAX 
+Length = 30;    # 0...yMAX 
 Width = 1;      # 0...xMAX
 # discretization
 Nx=1        # mesh divisions x-direction
 Ny=15       # mesh divisions y-direction
-dt=86400.0/5.0   # time step
+dt=7.57*10**6   # time step (see HM-test-case/parameters.py)
 Nt=10   # number of time steps
 Nci=3   # number of coupling iterations, fixed number is uncool, TODO convergence check
 # physical parameters
-rho = 2400 # bulk density
+rho = 2400.0 # bulk density
 rho_FR = 1000.0    # real fluid density 
 p_ref = 2.125*10**6    # reference pressure
 alpha_B = 1.0 # Biot coefficient
 phi = 0.3 # porosity
-K_S=10.0**9  # drained bulk modulus
-nu = 0;     # Poisson ratio
+K_S = 100.0*10**6  # drained bulk modulus
+nu = 0.0;     # Poisson ratio
 beta_p = 4.0*10**(-8) # fluid compressibility
 K = 50.0*10**(-15)   # permeability
 mu = 1.0*10**(-3)    # viscosity
 
-overburden = 2.0*2.125*10**6    # load on top
+overburden = -2.0*p_ref    # load on top (y-direction)
 acceleration_vector = fe.Constant((0, 0))   # causing body forces
 ZeroVector = fe.Constant((0,0))
 ZeroScalar = fe.Constant((0))	
@@ -42,6 +46,11 @@ E = K_S/(3*(1-2*nu));    # Young's modulus (bulk, drained)
 Lame1 = E*nu/(1+nu)/(1-2*nu)  
 Lame2 = E/2/(1+nu) 
 M = 1/( (alpha_B-phi)/K_S+phi*beta_p )# 1/M M...Biot modulus
+
+def max_norm_delta(f1, f2):
+    vertex_values_f1 = f1.compute_vertex_values(mesh)
+    vertex_values_f2 = f2.compute_vertex_values(mesh)
+    return np.max( np.abs(vertex_values_f1 - vertex_values_f2) )
 
 # Define strain and stress
 def epsilon(u):
@@ -52,7 +61,7 @@ def sigma_eff(u):
 ## Create mesh (simplex elements in 2D=triangles) and define function spaces
 mesh = fe.RectangleMesh(fe.Point(0, 0), fe.Point(Width, Length), Nx, Ny)
 VH = fe.FunctionSpace(mesh, 'P', 1)
-VM = fe.VectorFunctionSpace(mesh, 'P', degree=2)
+VM = fe.VectorFunctionSpace(mesh, 'P', 2)
 
 # IC undeformed, at rest, constant pressure everywhere
 p_0 = fe.Constant(p_ref)
@@ -68,7 +77,7 @@ sv_n = fe.interpolate(sv_0, VH) # hydrostatic total stress
 tol = 1E-14
 def topbottom(x, on_boundary):    
     return on_boundary and ( fe.near(x[1], Length, tol) or fe.near(x[1], 0.0, tol) )   
-bcH = fe.DirichletBC(VH, 2*p_ref, topbottom)  # drainage on top and bottom
+bcH = fe.DirichletBC(VH, p_ref, topbottom)  # drainage on top and bottom
 
 def bottom(x, on_boundary):    
     return on_boundary and (fe.near(x[1], 0.0, tol))   
@@ -82,7 +91,6 @@ topSD.mark(boundaries, 1)   # access via ds(1)
 ds = fe.ds(subdomain_data=boundaries)
 traction_topM = fe.Constant((0, overburden))		
 
-
 # Define variational H problem a(v,p)=L(v)
 pH = fe.TrialFunction(VH)
 vH = fe.TestFunction(VH)
@@ -94,7 +102,7 @@ aH = vH*rho_FR*((alpha_B**2)/K_S+1/M)*pH*fe.dx     # storage with p_(n+1)
 LH = vH*rho_FR*((alpha_B**2)/K_S+1/M)*p_n*fe.dx    # storage with p_n
 + dt*(rho_FR**2)*(K/mu)*fe.dot(fe.grad(vH), bH)*fe.dx    # body-force-driven Darcy
 - vH*rho_FR*(alpha_B/K_S)*(sv-sv_n)*fe.dx # M coupling (fixed-stress)
-# no Neumann BC (inflows)
+# no non-zero Neumann BC (inflows)
 # no sources
 
 # Define variational M problem a(v,p)=L(v)
@@ -121,15 +129,24 @@ u.assign(u_n)
 vtkfileM << (u, t)
 for n in range(Nt):
     t += dt
+    print(n+1,".step   t=",t)
     for nn in range(Nci):
+        
         fe.solve(aH == LH, p, bcH)
+        delta_p=max_norm_delta(p_n, p)
         p_n.assign(p)
+        
         fe.solve(aM == LM, u, bcM)
         sigma_v=fe.project( (1.0/3.0)*fe.tr(sigma_eff(u)) - alpha_B*p, VH)
         sv.assign(sigma_v)
+        delta_sv=max_norm_delta(sv_n, sv)
+        
+        print(delta_p, delta_sv)
+        
     sv_n.assign(sigma_v)
     vtkfileH << (p,t)
     vtkfileM << (u,t)
+
         
         
         
